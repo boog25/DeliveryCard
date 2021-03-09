8384b87cc3e5e73b52d50a67e40a6d64db444fa3
boog25 <voroshilov.d.v123@gmail.com>
2021-03-09 21:28:10 +0500

    Initial commit

---
+++ src/test/java/ru/netology/TestDelivery.java
@@ -0,0 +1,37 @@
+package ru.netology;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.exist;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class TestDelivery {
+
+    LocalDate date = LocalDate.now();
+    LocalDate newDays = date.plusDays(4);
+    DateTimeFormatter format = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
+    @Test
+    void shouldNewDelivery() {
+        open("http://localhost:9999/");
+        $("[placeholder='Город']").setValue("Уфа").click();
+        $(("[placeholder='Дата встречи']")).sendKeys("\b\b\b\b\b\b\b\b");
+        $("[placeholder='Дата встречи']").sendKeys(format.format(newDays));
+        $("[name='name']").setValue("Иванов Павел");
+        $("[name='phone']").setValue("+79122593411");
+        $("[class = 'checkbox__box']").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $(withText("Успешно!")).shouldBe(exist);
+        $(byText("Успешно!")).shouldBe(Condition.visible, Duration.ofSeconds(14));
+
+    }
+}
